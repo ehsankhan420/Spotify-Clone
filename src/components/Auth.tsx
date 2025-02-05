e0b9db2eec4e702b9dccec57889289185ab5c0f5
ehsankhan420 <rockstarabhi53060@gmail.com>
2025-02-06 00:39:25 +0500

    1st Commit
    changed the icon

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -23,9 +23,12 @@ export function Auth() {
         isDark ? 'bg-zinc-900' : 'bg-white'
       }`}>
         <div className="mb-8 text-center">
-          <h1 className={`text-3xl font-bold mb-2 ${isDark ? 'text-white' : 'text-gray-900'}`}>
-            Welcome to Musicify
-          </h1>
+        <h1 className="text-2xl md:text-3xl font-bold text-white text-center flex items-center justify-center gap-2">
+        <svg viewBox="0 0 16 16" className="w-8 h-8" fill="currentColor">
+          <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.669 11.538a.498.498 0 0 1-.686.165c-1.879-1.147-4.243-1.407-7.028-.77a.499.499 0 0 1-.222-.973c3.048-.696 5.662-.397 7.77.892a.5.5 0 0 1 .166.686zm.979-2.178a.624.624 0 0 1-.858.205c-2.15-1.321-5.428-1.704-7.972-.932a.625.625 0 0 1-.362-1.194c2.905-.881 6.517-.454 8.986 1.063a.624.624 0 0 1 .206.858zm.084-2.268C10.154 5.56 5.9 5.419 3.438 6.166a.748.748 0 1 1-.434-1.432c2.825-.857 7.523-.692 10.492 1.07a.747.747 0 1 1-.764 1.288z" />
+        </svg>
+        Spotify
+      </h1>
           <p className={isDark ? 'text-zinc-400' : 'text-gray-600'}>
             Sign in to continue
           </p>
